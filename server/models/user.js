d6f1a457c9f62236d87ae4838ec21f1e488c94fb
wankunpp <wankunpp@gmail.com>
2018-06-08 15:44:53 -0400

    add POST/ users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -67,7 +67,21 @@ UserSchema.statics.findByToken = function(token) {
         'tokens.token':token,
         'tokens.access': 'auth'
     });
-}
+};
+
+UserSchema.statics.findByCredentials = function(email, password){
+    var User = this;
+
+    return User.findOne({email}).then((user) =>{
+        if(!user) return Promise.reject();
+        return new Promise((resolve,reject) => {
+           bcrypt.compare(password,user.password, (err, res) =>{
+               if(res) resolve(user);
+               reject(err);
+           })
+        })
+    });
+};
 
 UserSchema.pre('save', function(next) {
     var user = this;
